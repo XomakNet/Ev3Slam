6504ec18780bbf1b17cd1b3322441f438d5fd8eb
Xomak <mail@xomak.net>
2017-11-26 11:24:36 +0300

    Add generated map to show it in assignment report

--- visualize.py
+++ visualize.py
@@ -4,7 +4,7 @@ import numpy as np
 __author__ = 'Xomak'
 
 
-data = np.loadtxt('test.csv', delimiter=',')
+data = np.loadtxt('map_full.csv', delimiter=',')
 #data = data[data[:, 0].argsort()]
 
 plt.plot(data[:, 0], data[:, 1], 'ro')
